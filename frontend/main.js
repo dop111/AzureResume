a89f44de473e8746d4ec6be0877f0d42e85c283e
Daniele <dani.palazzo1@gmail.com>
2023-09-29 17:06:21 +0100

    Deployed code

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener("DOMContentLoaded",(event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://azureresumevisitorcountfunction.azurewebsites.net/api/GetResumeCounter?code=VKKX-sG0YO-9rRvxZBZHqmA3yIg4qyncyCyaCSV5VvSbAzFuKiW0rg==';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
